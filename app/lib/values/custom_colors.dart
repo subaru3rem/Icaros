6d877f3725c983df08b340f389e131b385b0169f
subaru3rem <cainancosta6@gmail.com>
2023-01-08 02:55:49 -0300

    paleta de cores do app

---
+++ app/lib/values/custom_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class custom_colors{
+  Color _primary_color = Color.fromARGB(255, 11, 22, 37);
+
+  Color primary_color(){
+    return _primary_color;
+  }
+  Color secundary_color(){
+    return Colors.green;
+  }
+}
