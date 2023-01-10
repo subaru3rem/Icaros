e4caa3aca8dda929dbef60b59b00985d98bc8fd6
subaru3rem <cainancosta6@gmail.com>
2023-01-10 05:53:46 -0300

    variaveis agora são estaticas

--- app/lib/values/custom_colors.dart
+++ app/lib/values/custom_colors.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
 
 class custom_colors{
-  Color _primary_color = Color.fromARGB(255, 11, 22, 37);
-
-  Color primary_color(){
-    return _primary_color;
-  }
-  Color secundary_color(){
-    return Colors.green;
-  }
+  static const primary_color = Color.fromARGB(255, 11, 22, 37);
+  static const secundary_color = Colors.green;
 }
