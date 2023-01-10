23151821204643c753a1813d6d600994217bc6e0
subaru3rem <cainancosta6@gmail.com>
2023-01-10 05:51:33 -0300

    layout refeito e adição dos botoes de volume

--- app/lib/screens/multimidia.dart
+++ app/lib/screens/multimidia.dart
@@ -14,35 +14,81 @@ class Multimidia extends StatelessWidget{
     return Scaffold(
       appBar: AppBar(
         title: const Text("Multmidia"),
-        shape:  Border(
+        shape:  const Border(
           bottom: BorderSide(
-            color:custom_colors().secundary_color(),
+            color:custom_colors.secundary_color,
             width: 2
           )
         ),
       ),
-     body: Center(
-      child: Row(
+     body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
         children: <Widget>[
-          FloatingActionButton(
+          Container(
+            margin: const EdgeInsets.all(20),
+            height: 80,
+            width: 80,
+            child:FloatingActionButton(
             heroTag: 'prevtrack',
             onPressed: () => midia_command('prevtrack'),
             child: const Icon(Icons.skip_previous)
-          ),
-          FloatingActionButton(
+          )),
+          Container(
+            margin: const EdgeInsets.all(20),
+            height: 80,
+            width: 80,
+            child:FloatingActionButton(
             heroTag: 'playpause',
             onPressed: () => midia_command('playpause'),
             child: const Icon(Icons.play_arrow),
-          ),
-          FloatingActionButton(
+          )),
+          Container(
+            margin: const EdgeInsets.all(20),
+            height: 80,
+            width: 80,
+            child:FloatingActionButton(
             heroTag: 'nexttrack',
             onPressed: () => midia_command('nexttrack'),
             child: const Icon(Icons.skip_next),
-          )
+          )),
+        ],
+      ),
+      Row(
+        
+        children: [
+          Container(
+            margin: const EdgeInsets.all(20),
+            height: 80,
+            width: 80,
+            child:FloatingActionButton(
+            heroTag: 'volumedown',
+            onPressed: ()=> midia_command('volumedown'),
+            child: const Icon(Icons.volume_down),
+          )),
+          Container(
+            margin: const EdgeInsets.all(20),
+            height: 80,
+            width: 80,
+            child:FloatingActionButton(
+            heroTag: 'volumemute',
+            onPressed: ()=> midia_command('volumemute'),
+            child: const Icon(Icons.volume_off),
+          )),
+          Container(
+            margin: const EdgeInsets.all(20),
+            height: 80,
+            width: 80,
+            child:FloatingActionButton(
+            heroTag: 'volumeup',
+            onPressed: ()=> midia_command('volumeup'),
+            child: const Icon(Icons.volume_up),
+          )),
         ],
       )
-     )
+      ]
+      )
     );
   }
 }
