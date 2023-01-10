a94e57dabce46b72e345c97baf7a1eea21d5c5ce
subaru3rem <cainancosta6@gmail.com>
2023-01-10 05:56:14 -0300

    separação das funções e adição de duas funções

--- server/server.py
+++ server/server.py
@@ -3,6 +3,9 @@ import os
 import pyautogui as p
 from time import sleep
 import pygetwindow as gw
+import psutil as ps
+import subprocess as su
+from time import sleep
 
 app = Flask(__name__)
 
@@ -10,31 +13,40 @@ app = Flask(__name__)
 def home():
         command = request.get_json(force=True)
         if 'navegador' in command.keys():
-            window = gw.getWindowsWithTitle('Google Chrome')
-            if not window:
-                os.startfile('C:\Program Files\Google\Chrome\Application\chrome.exe')
-                sleep(2)
-                window = gw.getWindowsWithTitle('Google Chrome')
-            try:
-                window[0].activate()
-            except:
-                window[0].minimize()
-                window[0].maximize()
-                print('erro activate')
-            with p.hold('ctrl'):
-                p.press('l')
-            p.typewrite(command['navegador'])
-            p.press('enter')
-            return 'Accepted'
+           navegador(command['navegador'])
+           return '200'
         elif 'multimidia' in command.keys():
             p.press(command['tecla'])
             return '200'
-        elif 'rotina' in command.keys():
-            pass
+        elif 'shutdown' in command.keys():
+            shutdown(command['shutdown'])
+            return '200'
+        elif 'pc_info' in command.keys():
+            return cpu_info()
         else:
             return 'Failed'
-
-    
-
+def navegador(link):
+    window = gw.getWindowsWithTitle('Google Chrome')
+    if not window:
+        os.startfile('C:\Program Files\Google\Chrome\Application\chrome.exe')
+        sleep(2)
+        window = gw.getWindowsWithTitle('Google Chrome')
+    try:
+        window[0].activate()
+    except:
+        window[0].minimize()
+        window[0].maximize()
+    with p.hold('ctrl'):
+        p.press('l')
+    p.typewrite(link)
+    p.press('enter')
+def shutdown(parametro): 
+    os.system('shutdown '+parametro) 
+def cpu_info():
+    sleep(.5)
+    cpu = ps.cpu_percent(interval=None)
+    memory = ps.virtual_memory()[2]
+    host = su.check_output('hostname', text=True)
+    return {'host':host, 'cpu':f'{cpu}','memory':f'{memory}'}
 
 app.run(host='0.0.0.0', port=5000, debug=True)
