56a2c6a8208f1d40c3a94ed00fb3637cefe6526d
subaru3rem <cainancosta6@gmail.com>
2023-03-17 07:51:27 -0300

    busca por ip nativa

--- app/android/app/src/main/kotlin/com/example/hello/MainActivity.kt
+++ app/android/app/src/main/kotlin/com/example/hello/MainActivity.kt
@@ -1,6 +1,38 @@
 package com.example.hello
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import java.net.NetworkInterface
 
 class MainActivity: FlutterActivity() {
+  private val CHANNEL = "samples.flutter.dev/ip"
+
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    super.configureFlutterEngine(flutterEngine)
+    MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+      call, result ->
+      if (call.method == "teste"){
+        var ip = Ip()
+        result.success(ip)
+      }else{
+        result.notImplemented()
+      }
+    }
+  }
+  fun Ip(teste: Int=0):String{
+    val networkInterfaces = NetworkInterface.getNetworkInterfaces()
+    while (networkInterfaces.hasMoreElements()) {
+        val networkInterface = networkInterfaces.nextElement()
+        val hardwareAddress = networkInterface.hardwareAddress
+        if (hardwareAddress != null) {
+            return networkInterface.name + " " + hardwareAddress.joinToString(separator = ":")
+        }
+        
+    }
+    return "seila"
+
+  }
 }
+
