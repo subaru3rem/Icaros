c323cc0f524b3bfb6ed1342637c22409ac2e1fe8
subaru3rem <cainancosta6@gmail.com>
2023-08-31 15:15:37 -0300

    adicionando função na classe de ip

--- app/lib/values/values.dart
+++ app/lib/values/values.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
-class custom_colors{
+class CustomColors{
+  // ignore: constant_identifier_names
   static const primary_color = Color.fromARGB(255, 11, 22, 37);
+  // ignore: constant_identifier_names
   static const secundary_color = Colors.green;
 }
 class Ips{
   static String ip = '';
+
+  static bool validateIp(String ip){
+    RegExp ipRegex = RegExp(r'^(\d?\d?\d)\.(\d?\d?\d)\.(\d?\d?\d)\.(\d?\d?\d)');
+    if (ipRegex.hasMatch(ip) || ip.contains('.')) {
+      return true;
+    }
+    return false;
+  }
 }
 class Window{
   static String window = 'home';
